3c7df1b9b430d6cae86a8bea32a4d1945edb149f
한 승희 <alphahacker.han@gmail.com>
2017-06-28 17:36:42 +0900

    test 2

--- src/memoryManager.js
+++ src/memoryManager.js
@@ -31,7 +31,7 @@ var memoryManager = {
 							console.log("userContents:");
 							console.log(userContents);
 							memoryManager.getExtIndexList(userContents, extractedIndexList, currRemainMemory, function(){
-								console.log("extractedIndexList:");
+								console.log("extractedIndexList ["+userId+"]:");
 								console.log(extractedIndexList);
 								//rejected();
 								resolved(extractedIndexList);
@@ -149,28 +149,23 @@ var memoryManager = {
 			var eachContent = function (index) {
 				console.log("index : " + index);
 				var key = userContents[index];
-				var value;
+				console.log("key : " + key);
 				redisPool.dataMemory.get(key, function (err, result) {
 						if(err) console.log("fail to push the content from data memory in redis! ");
 						if(result == undefined || result == null){
 							cb();
 							return false;
-							console.log("test here1");
 						}
 						else {
 							//3. 추출 데이터 리스트에 추가
 							extractedIndexList.push({	index : index,
 																				data : result });
 
-							//4. 데이터 사이즈(length) 측정
-							value = result;
-
 							//5. 해당 사이즈를 메모리에 뺏을때 남는 메모리가 0보다 크면, break
-							if(currRemainMemory + value.length > 0){
+							if(currRemainMemory + result.length > 0){
 								//return extractedIndexList;
 								cb();
 								return false;
-								console.log("test here2");
 							}
 						}
 				});
