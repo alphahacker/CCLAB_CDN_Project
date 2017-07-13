e2f597f27fe444a344e32057d770ba4df87c7e7b
한 승희 <alphahacker.han@gmail.com>
2017-07-13 14:07:48 +0900

    express 디폴트 로거 비활성화

--- surrogate/app.js
+++ surrogate/app.js
@@ -36,7 +36,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.use(logger('dev'));
+//app.use(logger('dev'));
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 app.use(cookieParser());
